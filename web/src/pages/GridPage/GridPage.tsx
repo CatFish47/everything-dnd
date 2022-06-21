207b35c2f3c3055fc5eb744814cc604c884ebaec
Grant Cheng <chengxgrant@gmail.com>
2022-06-20 21:10:04 -0700

    re-setup tailwindcss

--- web/src/pages/GridPage/GridPage.tsx
+++ web/src/pages/GridPage/GridPage.tsx
@@ -1,17 +1,45 @@
 import { MetaTags } from '@redwoodjs/web'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Stage, Layer, RegularPolygon, Text } from 'react-konva'
 
 const GridPage = () => {
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
+  })
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    updateSize()
+  }, [])
+
+  const updateSize = () => {
+    setDimensions({
+      width: ref.current.offsetWidth,
+      height: ref.current.offsetHeight,
+    })
+  }
+
+  window.addEventListener('resize', updateSize)
+
   return (
     <>
       <MetaTags title="Grid" description="Grid page" />
 
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <RegularPolygon sides={6} radius={50} fill={`blue`} x={100} y={100} />
-        </Layer>
-      </Stage>
+      <div ref={ref} className="w-screen h-screen">
+        <Stage width={dimensions.width} height={dimensions.height}>
+          <Layer>
+            <RegularPolygon
+              sides={6}
+              radius={50}
+              fill={`blue`}
+              x={100}
+              y={100}
+            />
+          </Layer>
+        </Stage>
+      </div>
     </>
   )
 }
