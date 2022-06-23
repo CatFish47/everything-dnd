3bf6b8d7c2e9c1bd4f8995e33250e60e200a3776
Grant Cheng <chengxgrant@gmail.com>
2022-06-23 15:49:32 -0700

    installed daisyui, added window size hook to grid

---
+++ web/src/components/Honeycomb/Honeycomb.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef, useState } from 'react'
+import { RegularPolygon } from 'react-konva'
+
+type HoneycombProps = {
+  gridSize: number
+  tileSize: number
+}
+
+const Honeycomb = (props: HoneycombProps) => {
+  return <></>
+}
+
+export default Honeycomb
--- web/src/pages/GridPage/GridPage.tsx
+++ web/src/pages/GridPage/GridPage.tsx
@@ -3,32 +3,33 @@ import { useEffect, useRef, useState } from 'react'
 import { Stage, Layer, RegularPolygon, Text } from 'react-konva'
 
 const GridPage = () => {
-  const [dimensions, setDimensions] = useState({
-    width: 0,
-    height: 0,
-  })
+  const dimensions = useWindowSize();
+  // const [dimensions, setDimensions] = useState({
+  //   width: window.innerWidth,
+  //   height: window.innerHeight,
+  // })
 
-  const ref = useRef(null)
+  // useEffect(() => {
+  //   updateSize()
+  // }, [])
 
-  useEffect(() => {
-    updateSize()
-  }, [])
-
-  const updateSize = () => {
-    setDimensions({
-      width: ref.current.offsetWidth,
-      height: ref.current.offsetHeight,
-    })
-  }
-
-  window.addEventListener('resize', updateSize)
+  // const updateSize = () => {
+  //   setDimensions({
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   })
+  // }
 
   return (
     <>
       <MetaTags title="Grid" description="Grid page" />
 
-      <div ref={ref} className="w-screen h-screen">
-        <Stage width={dimensions.width} height={dimensions.height}>
+      <div className="w-screen h-screen grid grid-cols-4">
+        <Stage
+          width={(dimensions.width * 3) / 4}
+          height={dimensions.height}
+          className="col-span-3"
+        >
           <Layer>
             <RegularPolygon
               sides={6}
@@ -39,9 +40,42 @@ const GridPage = () => {
             />
           </Layer>
         </Stage>
+        <div className="bg-gray-100">
+          <div className="m-3">
+            <h1 className="text-3xl text-center">Menu</h1>
+          </div>
+        </div>
       </div>
     </>
   )
+}
+
+// Taken from https://usehooks.com/useWindowSize/
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
 
 export default GridPage
